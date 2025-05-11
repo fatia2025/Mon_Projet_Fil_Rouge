bea2993e7462c16c6e5cdb5cbee53bfb46c65b2b
fatia2025 <essaidifatiha@gmail.com>
2025-05-11 19:22:41 +0200

    feat(js no fini formulaire et boutons)

---
+++ js.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("formconect");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const prenom = document.getElementById("prenom").value;
+  const email = document.getElementById("email").value;
+});
